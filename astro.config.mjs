daeb6f558214498ddfdecf597be9e8fdbd6bee27
Joao Santos <jrmsantos15@gmail.com>
2025-11-02 20:40:56 +0100

    feat: adds docs build dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/core3triathlon-website/', // github repo name
+  outDir: './docs', // for github pages
+  trailingSlash: 'ignore', 
   vite: {
     plugins: [tailwindcss()]
   }
